1546745e29608163f94df9641d708ebe7fbbc47f
kaizoku-oh <garssallaoui.bayrem@gmail.com>
2021-05-14 15:32:26 +0100

    listening to both nodes

--- public/app.js
+++ public/app.js
@@ -3,12 +3,25 @@
 firebase.initializeApp(firebaseConfig);
 
 // Initialize an instance of Cloud Firestore
-var db = firebase.firestore();
+const db = firebase.firestore();
 
-// Listen for changes on a certain document
+const dht11Node = {};
+const rfidNode = {};
+
+// Listen for changes on a certain document in a collection
 db.collection("devices").doc("rfid-node")
     .onSnapshot((doc) => {
-        console.log("Current serial-number: ", doc.data().sn);
+        rfidNode.ts = new Date().getTime();
+        rfidNode.sn = doc.data().sn;
+        console.log("Current rfid serial-number: ", rfidNode);
+    });
+
+db.collection("devices").doc("dht11-node")
+    .onSnapshot((doc) => {
+        dht11Node.ts = new Date().getTime();
+        dht11Node.temperature = doc.data().temperature;
+        dht11Node.humidity = doc.data().humidity;
+        console.log("Current temperature-humidity: ", dht11Node);
     });
 
 // Add example
