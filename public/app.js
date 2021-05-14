87196b60c5c463c663cb17df4d446a2aa4c6ad3d
kaizoku-oh <garssallaoui.bayrem@gmail.com>
2021-05-15 00:36:22 +0100

    Added chartjs

--- public/app.js
+++ public/app.js
@@ -8,21 +8,77 @@ const db = firebase.firestore();
 const dht11Node = {};
 const rfidNode = {};
 
+const labels = [];
+
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'Temperature in °C',
+    backgroundColor: [
+      'rgba(105, 0, 132, .2)',
+    ],
+    borderColor: [
+      'rgba(200, 99, 132, .7)',
+    ],
+    borderWidth: 2,
+    data: [],
+  },
+  {
+    label: 'Humidity in %',
+    backgroundColor: [
+      'rgba(0, 137, 132, .2)',
+    ],
+    borderColor: [
+      'rgba(0, 10, 130, .7)',
+    ],
+    borderWidth: 2,
+    data: [],
+  }
+  ]
+};
+
+const config = {
+  type: 'line',
+  data,
+  options: {}
+};
+
+const myChart = new Chart(
+  document.getElementById('myChart').getContext('2d'),
+  config
+);
+
+function addData(chart, label, datasetIndex, data) {
+  chart.data.labels.push(label);
+  chart.data.datasets[datasetIndex].data.push(data);
+  chart.update();
+}
+
 // Listen for changes on a certain document in a collection
 db.collection("devices").doc("rfid-node")
-    .onSnapshot((doc) => {
-        rfidNode.ts = new Date().getTime();
-        rfidNode.sn = doc.data().sn;
-        console.log("Current rfid serial-number: ", rfidNode);
-    });
+  .onSnapshot((doc) => {
+    if (!rfidNode.timestamp) {
+      rfidNode.timestamp = new Date().getTime();
+    }
+    rfidNode.sn = doc.data().sn;
+    console.log("Received rfid object: ", rfidNode);
+  });
 
 db.collection("devices").doc("dht11-node")
-    .onSnapshot((doc) => {
-        dht11Node.ts = new Date().getTime();
-        dht11Node.temperature = doc.data().temperature;
-        dht11Node.humidity = doc.data().humidity;
-        console.log("Current temperature-humidity: ", dht11Node);
-    });
+  .onSnapshot((doc) => {
+    if (!dht11Node.timestamp) {
+      dht11Node.timestamp = new Date().getTime();
+    }
+    dht11Node.temperature = doc.data().temperature;
+    dht11Node.humidity = doc.data().humidity;
+    console.log("Received dht11 object: ", dht11Node);
+    addData(myChart, new Date(dht11Node.timestamp).toLocaleString(), 0, dht11Node.temperature);
+    addData(myChart, new Date(dht11Node.timestamp).toLocaleString(), 1, dht11Node.humidity);
+  });
+
+// setTimeout(function () {
+//   // Run this function after 3 seconds
+// }, 3000);
 
 // Add example
 // db.collection("users").add({
